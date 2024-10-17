8e374ec7fa8dba9e8b4eef3ae9888453bff3ac05
Glowupmatt <glowupmatt@gmail.com>
2024-10-17 14:49:48 -0700

    create hello world map

---
+++ src/main/java/com/matthew/springboot/myfirstwebapp/SayHelloController.java
@@ -0,0 +1,14 @@
+package com.matthew.springboot.myfirstwebapp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class SayHelloController {
+    @RequestMapping("say-hello")
+    @ResponseBody
+    public String sayHello() {
+        return "Hello, World!";
+    }
+}
